e2b5ab99edf4898c25e88c563bff09d7d77dacf2
Nikunj Gupta <ng2531@nyu.edu>
2022-06-13 01:59:36 -0400

    removed bugs; wrote code for saving models and added learning paramters

--- runner.py
+++ runner.py
@@ -1,35 +1,37 @@
 # General 
-import numpy as np, json, argparse, yaml 
+import json, argparse, yaml 
 
 # Epipolicy 
-# from epipolicy_environment import EpiEnv 
+from epipolicy_environment import EpiEnv 
 
 # Reinforcement Learning 
-import gym 
+import torch 
 from stable_baselines3.common.env_checker import check_env 
 from stable_baselines3 import SAC 
+from stable_baselines3.common.callbacks import CheckpointCallback 
 
 
+TOTAL_TIMESTEPS = 100_000
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 
 parser.add_argument(
     '--exp', 
-    default='exp_name', 
+    # default='exp_name', 
     type=str, 
     help='path to config file for RL algorithm'
 ) 
 
 parser.add_argument(
     '--config', 
-    default='configs/sample.yaml', 
+    # default='configs/sample.yaml', 
     type=str, 
     help='path to config file for RL algorithm'
 ) 
 
 parser.add_argument(
     '--scenario', 
-    default='jsons/warmup_scenario.json', 
+    # default='jsons/warmup_scenario.json', 
     type=str, 
     help='path to json file for scenario'
 ) 
@@ -39,8 +41,9 @@ args = parser.parse_args()
 
 scenario = json.loads(open(args.scenario, "r").read()) 
 
-# env = EpiEnv(scenario) 
-env = gym.make("Pendulum-v1")
+# env = gym.make("Pendulum-v1")
+
+env = EpiEnv(scenario) 
 check_env(env)
 
 
@@ -51,37 +54,50 @@ with open(args.config, "r") as stream:
 
 config = config[args.exp]
 
+
 model = SAC(
     policy="MlpPolicy", 
     env=env, 
     learning_rate=config['learning_rate'], 
-    buffer_size=config['buffer_size'], 
-    learning_starts=config['learning_starts'], 
-    batch_size=config['batch_size'], 
+    buffer_size=int(config['buffer_size']), 
+    learning_starts=int(config['learning_starts']), 
+    batch_size=int(config['batch_size']), 
     tau=config['tau'], 
     gamma=config['gamma'], 
-    train_freq=config['train_freq'], 
-    gradient_steps=config['gradient_steps'], 
+    train_freq=int(config['train_freq']), 
+    gradient_steps=int(config['gradient_steps']), 
     action_noise=None, 
     replay_buffer_class=None, 
     replay_buffer_kwargs=None, 
     optimize_memory_usage=False, 
     ent_coef='auto', 
-    target_update_interval=config['target_update_interval'], 
+    target_update_interval=int(config['target_update_interval']), 
     target_entropy=config['target_entropy'], 
     use_sde=False, 
     sde_sample_freq=-1, 
     use_sde_at_warmup=False, 
     tensorboard_log='/'.join(['summaries', args.scenario.split('/')[-1].split('.')[0], args.exp]), 
     create_eval_env=False, 
-    policy_kwargs=None, 
+    policy_kwargs=dict(
+        activation_fn=torch.nn.ReLU, 
+        net_arch=[int(config['first_hidden_size']), int(config['second_hidden_size'])] 
+    ), 
     verbose=1, 
     seed=config['seed'], 
     device='auto', 
     _init_setup_model=True
 )
 
-model.learn(total_timesteps=1000, log_interval=4)
-
-
-model.save('/'.join(['summaries', args.scenario.split('/')[-1].split('.')[0], args.exp, args.exp])) 
+model.learn(
+    total_timesteps=TOTAL_TIMESTEPS, 
+    log_interval=4,
+    callback=CheckpointCallback(
+        save_freq=TOTAL_TIMESTEPS/10, 
+        save_path='/'.join([
+            'summaries', 
+            args.scenario.split('/')[-1].split('.')[0], 
+            args.exp, 
+            'model_checkpoints'
+        ]) 
+    )
+)
