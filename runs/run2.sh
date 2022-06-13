ba5ade90d744fc55b2ff3a93ac6acb1e7955fa42
Nikunj Gupta <ng2531@nyu.edu>
2022-06-13 02:02:02 -0400

    runs for hpc

---
+++ runs/run2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16 
+#SBATCH --time=2:00:00
+#SBATCH --mem=40GB
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=exp_3
+#SBATCH --output=exp_3.out
+
+module load python/intel/3.8.6
+module load openmpi/intel/4.0.5
+
+source ./venv/bin/activate
+time python3 runner.py  --exp exp_3 --config configs/config.yaml --scenario jsons/SIRV_A.json
